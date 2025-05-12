4ce6ebe19a4891f957e512d7a305e0043efeca6f
ITSNE01 <rabie.haddar1@gmail.com>
2025-05-12 14:59:13 +0100

    task_1 --

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,21 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
 
-$(function() {
-  $('body')
-    .append('<p>ALX Dashboard</p>')
-    .append('<p>Dashboard data for the students</p>')
-    .append("<button id='start-btn'>Click here to get started</button>")
-    .append("<p id='count'></p>")
-    .append('<p>\u00A9 Copyright - ALX</p>');
+// Add elements to the page
+$(document).ready(function() {
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - ALX</p>');
 
+  // Counter functionality
   let count = 0;
-  const updateCounter = () => {
-    count += 1;
+  function updateCounter() {
+    count++;
     $('#count').text(`${count} clicks on the button`);
-  };
+  }
 
-  $('#start-btn').on('click', debounce(updateCounter, 300));
+  // Bind debounced function to button click
+  $('button').on('click', _.debounce(updateCounter, 300));
 });
