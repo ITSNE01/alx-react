8d053bc122f63c614baf4ba269c71641fda260b6
ITSNE01 <rabie.haddar1@gmail.com>
2025-05-20 19:10:38 +0100

    task 11

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Notifications from './Notifications';
 
-describe('<Notifications /> markAsRead', () => {
-  it('markAsRead logs the correct message', () => {
-    const wrapper = shallow(<Notifications displayDrawer listNotifications={[]} />);
-    const instance = wrapper.instance();
-    const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-    instance.markAsRead(42);
-    expect(consoleSpy).toHaveBeenCalledWith('Notification 42 has been marked as read');
-    consoleSpy.mockRestore();
+describe('<Notifications /> shouldComponentUpdate', () => {
+  let wrapper;
+  const list1 = [
+    { id: 1, type: 'default', value: 'New course available' },
+  ];
+  const list2 = [
+    ...list1,
+    { id: 2, type: 'urgent', value: 'New resume available' },
+  ];
+
+  beforeEach(() => {
+    // Spy on render to count how many times component actually renders
+    jest.spyOn(Notifications.prototype, 'render');
+    wrapper = mount(<Notifications displayDrawer listNotifications={list1} />);
+    // clear initial render count
+    Notifications.prototype.render.mockClear();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    wrapper.unmount();
+  });
+
+  it('does NOT rerender when props.listNotifications length stays the same', () => {
+    wrapper.setProps({ listNotifications: list1 });
+    expect(Notifications.prototype.render).not.toHaveBeenCalled();
+  });
+
+  it('does rerender when props.listNotifications length increases', () => {
+    wrapper.setProps({ listNotifications: list2 });
+    expect(Notifications.prototype.render).toHaveBeenCalled();
   });
 });
