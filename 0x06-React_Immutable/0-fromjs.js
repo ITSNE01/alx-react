839ad0801845906c1f506b869c09958c9361374f
ITSNE01 <rabie.haddar1@gmail.com>
2025-06-04 18:18:37 +0100

    0-fromjs.js

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,11 @@
+import { fromJS } from 'immutable';
+
+/**
+ * Converts a plain JavaScript object into an Immutable Map.
+ *
+ * @param {Object} obj - The source object.
+ * @returns {Map} An Immutable Map representation of the object.
+ */
+export default function getImmutableObject(obj) {
+  return fromJS(obj);
+}
