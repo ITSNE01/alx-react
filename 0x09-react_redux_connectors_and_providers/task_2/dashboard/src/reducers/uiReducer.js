fcf9adc24ca0254f9d880782acdd6bd0bcca6b60
ITSNE01 <rabie.haddar1@gmail.com>
2025-06-23 20:42:06 +0100

    task 11

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -2,16 +2,16 @@ import { Map } from 'immutable';
 import {
   DISPLAY_NOTIFICATION_DRAWER,
   HIDE_NOTIFICATION_DRAWER,
+  LOGIN,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
   LOGOUT,
 } from '../actions/uiActionTypes';
 
-// Initial UI state as an Immutable Map
 export const initialState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn:             false,
-  user:                       {},
+  user:                       null,
 });
 
 /**
@@ -27,12 +27,18 @@ export default function uiReducer(state = initialState, action = {}) {
       return state.set('isNotificationDrawerVisible', true);
     case HIDE_NOTIFICATION_DRAWER:
       return state.set('isNotificationDrawerVisible', false);
+    case LOGIN:
+      return state
+        .set('user', action.user)
+        .set('isUserLoggedIn', true);
     case LOGIN_SUCCESS:
       return state.set('isUserLoggedIn', true);
     case LOGIN_FAILURE:
       return state.set('isUserLoggedIn', false);
     case LOGOUT:
-      return state.set('isUserLoggedIn', false);
+      return state
+        .set('user', null)
+        .set('isUserLoggedIn', false);
     default:
       return state;
   }
