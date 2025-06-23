ea6e2dba831c8c10236da79d754c219e738824c6
ITSNE01 <rabie.haddar1@gmail.com>
2025-06-23 19:44:36 +0100

    task_1

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import uiReducer from './reducers/uiReducer';
 import App from './App/App';
 
-console.log('📦 index.js loaded');
+const store = createStore(combineReducers({ uiReducer }));
 
 const container = document.getElementById('root');
-if (!container) {
-  throw new Error('No #root element found in index.html');
-}
-createRoot(container).render(<App />);
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
